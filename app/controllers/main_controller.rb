6bab8a887dbd92ed9f9a48fa9f9a291e0c43c156
ehaala <ejhaala@gmail.com>
2017-11-13 16:11:50 -0800

    form working with api

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
   def index
+  end
+
+  def show
 
 		uri = URI('https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect')
 		uri.query = URI.encode_www_form({
@@ -14,13 +17,14 @@ class MainController < ApplicationController
 		# Request headers
 		request['Content-Type'] = 'application/json'
 		# NOTE: Replace the "Ocp-Apim-Subscription-Key" value with a valid subscription key.
-		request['Ocp-Apim-Subscription-Key'] = ''
+		request['Ocp-Apim-Subscription-Key'] = ENV['API_KEY']
 		# Request body
 		# request.body = "{\"url\":\"http://images.performgroup.com/di/library/sporting_news/39/82/lebron-james-yell-052217-getty-ftrjpg_1biyl0msydg5s1dqs6vm31t7aq.jpg?t=775653340&w=960&quality=70\"}"
 		request1 = "{\"url\":\""
-		request2 = "https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/09/15/dirk_nowitzki_marquee_.jpg"
+		@url = params[:url]
+		# url = "https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/09/15/dirk_nowitzki_marquee_.jpg"
 		request3 = "\"}"
-		request.body = request1 + request2 + request3
+		request.body = request1 + @url + request3
 
 		response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
 		    http.request(request)
@@ -29,4 +33,5 @@ class MainController < ApplicationController
 		@data = JSON.parse(response.body)
 
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'main#index'
 
+  get 'results' => 'main#show'
+
+  post 'results' => 'main#show'
+
   get 'login' => 'sessions#new'
 
   post 'login' => 'sessions#create'
@@ -8,5 +12,6 @@ Rails.application.routes.draw do
   get 'logout' => 'sessions#destroy'
 
   resources :user
+  resources :main
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
